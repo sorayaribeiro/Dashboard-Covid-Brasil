4aba03d12da01b91cbd4cef5d376b92a805dc5e0
Soraya Ribeiro <soraya.alr@gmail.com>
2022-02-17 08:56:41 -0300

    Inicio da api
    Fechamento da busca por estados, inicio do carregamento das api's

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ const _elements = {
     selectList: document.querySelector(".state-select-list"),
     selectToggleIcon: document.querySelector(".state-select-toggle__icon"),
     selectSearchBox: document.querySelector(".state-select-list__search"),
-    selectStateSelected: document.querySelector(".state-select-toggle__label"),
+    selectStateSelected: document.querySelector(".state-select-toggle__state-selected"),
     confirmed: document.querySelector(".info__total--confirmed"),
     deaths: document.querySelector(".info__total--deaths"),
     deathsDescription: document.querySelector(".data-box__description"),
@@ -38,18 +38,50 @@ _elements.stateSelectToggle.addEventListener("click", () => {
 });
 
 _elements.selectOptions.forEach(item => {
-
+    item.addEventListener("click", () => {
+        _elements.selectStateSelected.innerText = item.innerText;
+        _data.id = item.getAttribute("data-id");
+        _elements.stateSelectToggle.dispatchEvent(new Event("click"));
+    });
 });
 
 _elements.selectSearchBox.addEventListener("keyup", (e) => {
+    const search = e.target.value.toLowerCase();
+
+    for(const item of _elements.selectOptions){
+        const state = item.innerText.toLowerCase();
+
+        if(state.includes(search)){
+            item.classList.remove("state-select-list__item--hide");
+        }
+        else{
+            item.classList.add("state-select-list__item--hide");
+        }
+    }
 
 });
 
-const request = (api, id) => {
+const request = async (api, id) => {
+    try{
+        const url = api + id;
 
+        const data = await fetch(url);
+        const json = await data.json();
+
+        return json;
+    }
+    catch(e){
+        console.log(e);
+    }
 }
 
 const loadData = (id) => {
+    _data.confirmed = request(_api.confirmed, id);
+    _data.deaths = request(_api.deaths, id);
+    _data.vaccinated = request(_api.vaccinated, id);
+    _data.vaccinatedInfo = request(_api.vaccinatedInfo, "");
+
+    updateCards();
 
 }
 
@@ -70,7 +102,7 @@ const createCharts = () => {
 }
 
 const updateCards = () => {
-
+    _elements.confirmed.innerText = _data.confirmed[_data.confirmed.length-1]["total_de_casos"];
 }
 
 const updateCharts = () => {
